fe56c01b1f3b8dac7d612f512e2960e5dee0cd11
jhenrique09 <jhenrique09.mcz@hotmail.com>
2019-09-07 03:16:18 -0300

    vendor: Add soong variable for TARGET_CAMERA_BOOTTIME_TIMESTAMP
    Change-Id: Ief38c409812ff0819a4906c3e1c5037532f47f18

--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -12,6 +12,7 @@ $(call add_json_str_omitempty, Target_process_sdk_version_override, $(TARGET_PRO
 $(call add_json_str, Target_shim_libs, $(TARGET_LD_SHIM_LIBS))
 $(call add_json_bool, Uses_generic_camera_parameter_library, $(if $(TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY),false,true))
 $(call add_json_bool, Uses_qcom_bsp_legacy, $(filter true,$(TARGET_USES_QCOM_BSP_LEGACY)))
+$(call add_json_bool, Needs_camera_boottime_timestamp, $(filter true,$(TARGET_CAMERA_BOOTTIME_TIMESTAMP)))
 
 # This causes the build system to strip out the last comma in our nested struct, to keep the JSON valid.
 _json_contents := $(_json_contents)__SV_END
