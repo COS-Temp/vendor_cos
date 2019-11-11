4a89c5219d6ccfa2ed824ba630ffc9a98b4784c1
Sagar Shah <sagarshah2326@gmail.com>
2019-11-11 14:50:48 +0100

    vendor: Make Qcom Targets inclusion inline with Lineage
    Signed-off-by: Sagar Shah <sagarshah2326@gmail.com>

--- build/core/qcom_target.mk
+++ build/core/qcom_target.mk
@@ -19,7 +19,6 @@ $(call project-set-path,qcom-$(2),$(strip $(path)))
 endef
 
 ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
-BOARD_USES_QTI_HARDWARE := true
 
 $(call set-device-specific-path,AUDIO,audio,hardware/qcom/audio-caf/$(QCOM_HARDWARE_VARIANT))
 $(call set-device-specific-path,DISPLAY,display,hardware/qcom/display-caf/$(QCOM_HARDWARE_VARIANT))
@@ -32,6 +31,8 @@ $(call set-device-specific-path,SENSORS,sensors,hardware/qcom/sensors)
 $(call set-device-specific-path,LOC_API,loc-api,vendor/qcom/opensource/location)
 $(call set-device-specific-path,DATASERVICES,dataservices,vendor/qcom/opensource/dataservices)
 $(call set-device-specific-path,POWER,power,hardware/qcom/power)
+$(call set-device-specific-path,THERMAL,thermal,hardware/qcom/thermal)
+$(call set-device-specific-path,VR,vr,hardware/qcom/vr)
 
 $(call wlan-set-path-variant,wlan-caf)
 $(call bt-vendor-set-path-variant,bt-caf)
